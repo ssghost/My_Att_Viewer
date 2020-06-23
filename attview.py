1e1626df15c31c542c00a1e2872bd9f7d03dfba8
Shuang Song <6882971+ssghost@users.noreply.github.com>
2020-06-23 23:15:22 +0800

    Update attview.py

--- attview.py
+++ attview.py
@@ -7,27 +7,30 @@ class Visualizer:
     def __init__(self):
         self.model = None
         self.attention = None
+        self.input = None
         self.map = None
 
     def read(self, modelpath):
         self.model = load_model(modelpath)
         self.attention = self.model.get_layer('attention_vec').output
-
+        self.input = self.model.get_layyer(index = 0).input
+        
     def color_cv(self, s):
         r = 255-int(s.val*255)
         color = '#%02x%02x%02x' % (255,r,r)
         return color
 
-    def create_map(self, seq, figsize):
+    def create_map(self, seq, figsize, savepath):
         class CVpair(object):
             def __init__(self, char, val):
                 self.char = char
                 self.val = val
             def __str__(self):
                 return self.char 
-        att_out = self.attention[0][-len(seq.in_tokens):]
-        cvpairs = [CVpair(c,v) for c,v in zip(seq.dec, att_out)]
+        att_out = self.attention[0][-len(self.input):]
+        cvpairs = [CVpair(c,v) for c,v in zip(self.input, att_out)]
         cdf = pd.DataFrame(cvpairs).transpose()
         self.map = cdf.style.applymap(self.color_cv)
         fig = self.map.plot(size = figsize).get_figure()
-        fig.savefig('attmap.pdf')
+        fig.savefig(savepath)
+
