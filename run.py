958cc12904b8d7fc332f32466e5af62415a855ce
Shuang Song <6882971+ssghost@users.noreply.github.com>
2020-06-23 23:44:09 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import getopt, sys
 from attview import Visualizer
-from seqnet import Seqnet
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 't:o:l:f:s:', ['text=','outpath=','modelpath=','figsize=','savepath='])
+        opts, args = getopt.getopt(sys.argv[1:], 'l:f:s:', ['modelpath=','figsize=','savepath='])
     except getopt.GetoptError as err:
         print(err) 
         sys.exit()
@@ -12,11 +11,7 @@ def main():
     tpath, opath, loadpath = '','',''
     
     for o, a in opts:
-        if o in ('-t', '--text') and type(a)==str:
-            tpath = a
-        elif o in ('-o', '--outpath') and type(a)==str:
-            opath = a 
-        elif o in ('-l', '--modelpath') and type(a)==str:
+        if o in ('-l', '--modelpath') and type(a)==str:
             loadpath = a
         elif o in ('-f', '--figsize') and type(a)==int:
             figsize = a 
@@ -25,13 +20,7 @@ def main():
         else:
             assert False, 'unhandled option'    
     
-    seq = Seqnet()
     vis = Visualizer()
-
-    if loadpath != None:
-        seq.load_model(loadpath)
-        
-    seq.test(tpath=tpath,opath=opath)
     vis.read(loadpath)
     vis.create_map(figsize, savepath)
     
