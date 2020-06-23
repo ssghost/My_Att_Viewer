48dbffaeaffa0b11d0f4d28fa613acf19675c57a
Shuang Song <6882971+ssghost@users.noreply.github.com>
2020-06-23 23:45:04 +0800

    Update dlmodel.sh

--- dlmodel.sh
+++ dlmodel.sh
@@ -1,1 +1,1 @@
-gh repo clone ssghost/My_Attention_Seqnet && cp My_Attention_Seqnet/seqnet.py . && cp -r My_Attention_Seqnet/*.h5 .
+gh repo clone ssghost/My_Attention_Seqnet && cp My_Attention_Seqnet/seqnet.py . && My_Attention_Seqnet/test.py && cp -r My_Attention_Seqnet/*.h5 .
